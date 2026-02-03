73ad7a0688ebac602390553e449ac4bdce6cdb9b
aliaftabsheikh <aliaftabsheikh1@gmail.com>
2026-02-03 16:31:59 +0500

    'class-26'

---
+++ class-26/README.md
@@ -0,0 +1,3 @@
+Chat App: https://www.notion.so/Real-Time-Chat-App-with-Express-js-Socket-IO-Beginner-Advanced-Guide-2fceb965516a80298628c2e62224d001
+
+Cloudinary and multer : https://www.notion.so/Upload-Your-First-File-to-Cloudinary-Beginner-Guide-2f7eb965516a8080b992c7c0316375e1?source=copy_link
---
+++ class-26/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.static('public'))
+
+// io.on('connection', (socket) => {
+//   console.log('User connected:', socket.id)
+
+//   socket.on('message', (msg) => {
+//     io.emit('message', msg)
+//   })
+
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected')
+//   })
+// })
+
+
+// Room-based messaging
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (room) => {
+    socket.join(room)
+  })
+
+  socket.on('room-message', ({ room, message }) => {
+
+    io.to(room).emit('message', message)
+
+  })
+})
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000')
+})
